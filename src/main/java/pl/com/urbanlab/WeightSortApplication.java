3fca3c521e3af46a005be3ec2411208dc0df9f42
andrzeju <andrzej.urban.pl@gmail.com>
2017-01-31 23:50:29 +0100

    ng & spring-boot starter

---
+++ src/main/java/pl/com/urbanlab/WeightSortApplication.java
@@ -0,0 +1,12 @@
+package pl.com.urbanlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightSortApplication.class, args);
+	}
+}
